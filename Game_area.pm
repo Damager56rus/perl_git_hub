08fa56bc0b5aeabfa128077bd10dc062afa5c1d8
Дмитрий Губачев <gubachev@reg.ru>
2017-09-26 18:31:54 +0400

    Реализация сущностей для SpaceLife v0.7

--- Game_area.pm
+++ Game_area.pm
@@ -1,55 +1,169 @@
 package Game_area;
 
 use Modern::Perl;
+use DBI;
 
 sub new {
-	my ( $class, $number, $size_x, $size_y ) = @_;
+	my ( $class, $number, $size_x, $size_y , $id ) = @_;
 	my $self = { };
 
 	if ( $number =~ /^\d+$/ ) {
-		 $self->{ number } = $number;
+		 $self->{number} = $number;
 	}
 	else { die "Invalid value for number!"; }
 
 	if ( $size_x =~ /^\d+$/ ) {
-		 $self->{ size_x } = $size_x;
+		 $self->{size_x} = $size_x;
 	}
 	else { die "Invalid value for size_x!"; }
 
 	if ( $size_y =~ /^\d+$/ ) {
-		 $self->{ size_y } = $size_y;
+		 $self->{size_y} = $size_y;
 	}
 	else { die "Invalid value for size_y!"; }
 
 	bless $self, $class;
+
+	my $request = "INSERT INTO game_area( number, size_x, size_y ) 
+	               VALUES( '$self->{number}', '$self->{size_x}', '$self->{size_y}' )";
+    
+    db_write_area( $self, $request );
 	
 	return $self;
 }
 
-sub get_attributes {
+sub db_connect {
+	my $dbh = DBI->connect( 'DBI:mysql:locations', 'user', 'password' )
+       or die "Error connecting to database";
+    
+    return $dbh;
+}
+
+sub db_read_area {
+    my ( $class, $id ) = @_;
+    my $dbh = db_connect( );
+    my $sth = $dbh->prepare( "SELECT * FROM game_area WHERE id = '$id'" );
+    
+    $sth->execute;
+    
+    my $db_data = $sth->fetchrow_hashref;
+    
+    $sth->finish;
+    
+    $dbh->disconnect;
+
+    bless $db_data, $class;
+
+    return $db_data;
+}
+
+sub db_write_area {
+    my ( $self, $request ) = @_;
+    my $dbh = db_connect( );
+    my $sth = $dbh->prepare( $request ); # подготовить запрос к выполнению
+    
+    $sth->execute;
+
+    $dbh->disconnect;
+}
+
+sub list_of_areas {
+    my $dbh = db_connect( );
+    my $sth = $dbh->prepare( "SELECT * FROM game_area" );
+    
+    $sth->execute;
+    
+    while ( my $db_data = $sth->fetchrow_hashref ) {
+            say "---Game_area---";
+            say "id: ", $db_data->{id};
+            say "number: ", $db_data->{number};
+            say "size_x: ", $db_data->{size_x};
+            say "size_y: ", $db_data->{size_y};
+    }
+
+    $sth->finish;
+    
+    $dbh->disconnect;
+}
+
+sub get_area_info {
 	my $self = shift;
 
-	return $self->{number},
-	       $self->{size_x},
-	       $self->{size_y};
+    say "id: ", $self->{id};
+	say "number: ", $self->{number};
+	say "size_x: ", $self->{size_x};
+	say "size_y: ", $self->{size_y};
+}
+
+sub get_id {
+	my $self = shift;
+
+	return $self->{id};
 }
 
 sub get_number {
 	my $self = shift;
 	
-	return $self->{ number };
+	return $self->{number};
+}
+
+sub set_number {
+	my ( $self, $number ) = @_;
+	my $request = "UPDATE game_area SET number = '$number' WHERE id = '$self->{id}'";
+
+	if ( $number =~ /^\d+$/ ) {
+		 
+         db_write_area( $self, $request );
+
+		 return $self->{number} = $number;
+	}
+	else { die "Invalid value for number!"; }
 }
 
 sub get_size_x {
 	my $self = shift;
 	
-	return $self->{ size_x };
+	return $self->{size_x};
+}
+
+sub set_size_x {
+	my ( $self, $size_x ) = @_;
+	my $request = "UPDATE game_area SET size_x = '$size_x' WHERE id = '$self->{id}'";
+
+	if ( $size_x =~ /^\d+$/ ) {
+		 
+         db_write_area( $self, $request );
+
+		 return $self->{size_x} = $size_x;
+	}
+	else { die "Invalid value for size_x!"; }
 }
 
 sub get_size_y {
 	my $self = shift;
 	
-	return $self->{ size_y };
+	return $self->{size_y};
+}
+
+sub set_size_y {
+	my ( $self, $size_y ) = @_;
+	my $request = "UPDATE game_area SET size_y = '$size_y' WHERE id = '$self->{id}'";
+
+	if ( $size_y =~ /^\d+$/ ) {
+		 
+         db_write_area( $self, $request );
+
+		 return $self->{size_y} = $size_y;
+	}
+	else { die "Invalid value for size_y!"; }
+}
+
+sub get_size {
+	my $self = shift;
+	my $size_x = get_size_x( $self );
+	my $size_y = get_size_y( $self );
+	
+	return $size_x, $size_y;
 }
 
 sub spawn_enimes {
--- Location_object.pm
+++ Location_object.pm
@@ -1,9 +1,10 @@
 package Location_object;
 
 use Modern::Perl;
+use DBI;
 
 sub new {
-	my ( $class, $type, $name, $coordinate_x, $coordinate_y, $area_number ) = @_;
+	my ( $class, $type, $name, $coordinate_x, $coordinate_y, $area_number, $id ) = @_;
 	my $self = { };
 
     if ( $type =~ /^planet$|^station$/i ) {
@@ -31,19 +32,86 @@ sub new {
 	}
 	else { die "Invalid value for area_number!"; }
 
+    my $request = "INSERT INTO location_object( type, name, coordinate_x, coordinate_y, area_number ) 
+                   VALUES( '$self->{type}', '$self->{name}' ,'$self->{coordinate_x}', '$self->{coordinate_y}', '$self->{area_number}' )";
+	db_write_object( $self, $request );
+
 	bless $self, $class;
 	
 	return $self;
 }
 
-sub get_attributes {
+sub db_connect {
+	my $dbh = DBI->connect( 'DBI:mysql:locations', 'root', 'w12345%@' )
+       or die "Error connecting to database";
+    
+    return $dbh;
+}
+
+sub db_read_object {
+    my ( $class, $id ) = @_;
+    my $dbh = db_connect( );
+    my $sth = $dbh->prepare( "SELECT * FROM location_object WHERE id = '$id'" );
+    
+    $sth->execute;
+    
+    my $db_data = $sth->fetchrow_hashref;
+    
+    $sth->finish;
+    
+    $dbh->disconnect;
+
+    bless $db_data, $class;
+
+    return $db_data;
+}
+
+sub db_write_object {
+    my ( $self, $request ) = @_;
+    my $dbh = db_connect( );
+    my $sth = $dbh->prepare( $request ); # подготовить запрос к выполнению
+    
+    $sth->execute;
+  
+    $dbh->disconnect;
+}
+
+sub list_of_objects {
+    my $dbh = db_connect( );
+    my $sth = $dbh->prepare( "SELECT * FROM location_object" );
+    
+    $sth->execute;
+    
+    while ( my $db_data = $sth->fetchrow_hashref ) {
+            say "---Location_object---";
+            say "id: ", $db_data->{id};
+            say "type: ", $db_data->{type};
+            say "name: ", $db_data->{name};
+            say "coordinate_x: ", $db_data->{coordinate_x};
+            say "coordinate_y: ", $db_data->{coordinate_y};
+            say "area_number: ", $db_data->{area_number};
+    }
+
+    $sth->finish;
+    
+    $dbh->disconnect;
+}
+
+sub get_object_info {
 	my $self = shift;
 
-	return $self->{type},
-	       $self->{name},
-	       $self->{coordinate_x},
-	       $self->{coordinate_y},
-	       $self->{area_number};
+    say "id: ", $self->{id};
+	say "type: ", $self->{type};
+	say "name: ", $self->{name};
+	say "coordinate_x: ", $self->{coordinate_x};
+	say "coordinate_y: ", $self->{coordinate_y};
+	say "area_number: ", $self->{area_number};
+}
+
+sub get_id {
+	my $self = shift;
+
+	return $self->{id};
 }
 
 sub get_type {
@@ -52,10 +120,36 @@ sub get_type {
 	return $self->{type};
 }
 
+sub set_type {
+	my ( $self, $type ) = @_;
+	my $request = "UPDATE location_object SET type = '$type' WHERE id = '$self->{id}'";
+
+	if ( $type =~ /^planet$|^station$/i ) {
+		 
+         db_write_object( $self, $request );
+
+		 return $self->{type} = $type;
+	}
+	else { die "Invalid value for type!"; }
+}
+
 sub get_name {
 	my $self = shift;
 	
 	return $self->{name};
+}
+
+sub set_name {
+	my ( $self, $name ) = @_;
+	my $request = "UPDATE location_object SET name = '$name' WHERE id = '$self->{id}'";
+
+	if ( $name =~ /^\w+$/ ) {
+		 
+         db_write_object( $self, $request );
+
+		 return $self->{name} = $name;
+	}
+	else { die "Invalid value for name!"; }
 }
 
 sub get_coordinate_x {
@@ -64,23 +158,63 @@ sub get_coordinate_x {
 	return $self->{coordinate_x};
 }
 
+sub set_coordinate_x {
+	my ( $self, $coordinate_x ) = @_;
+	my $request = "UPDATE location_object SET coordinate_x = '$coordinate_x' WHERE id = '$self->{id}'";
+
+	if ( $coordinate_x =~ /^\d+$/ ) {
+		 
+         db_write_object( $self, $request );
+
+		 return $self->{coordinate_x} = $coordinate_x;
+	}
+	else { die "Invalid value for coordinate_x!"; }
+}
+
 sub get_coordinate_y {
 	my $self = shift;
 	
 	return $self->{coordinate_y};
 }
 
+sub set_coordinate_y {
+	my ( $self, $coordinate_y ) = @_;
+	my $request = "UPDATE location_object SET coordinate_y = '$coordinate_y' WHERE id = '$self->{id}'";
+
+	if ( $coordinate_y =~ /^\d+$/ ) {
+		 
+         db_write_object( $self, $request );
+
+		 return $self->{coordinate_y} = $coordinate_y;
+	}
+	else { die "Invalid value for coordinate_y!"; }
+}
+
 sub get_coordinates {
 	my $self = shift;
+    my $coordinate_x = get_coordinate_x( $self );
+    my $coordinate_y = get_coordinate_y( $self );
 
-	return $self->{coordinate_x},
-	       $self->{coordinate_y};
+	return $coordinate_x, $coordinate_y;
 }
 
 sub get_area_number {
 	my $self = shift;
 	
 	return $self->{area_number};
+}
+
+sub set_area_number {
+	my ( $self, $area_number ) = @_;
+	my $request = "UPDATE location_object SET area_number = '$area_number' WHERE id = '$self->{id}'";
+
+	if ( $area_number =~ /^\d+$/ ) {
+		 
+         db_write_object( $self, $request );
+
+		 return $self->{area_number} = $area_number;
+	}
+	else { die "Invalid value for area_number!"; }
 }
 
 sub move_by_object {
