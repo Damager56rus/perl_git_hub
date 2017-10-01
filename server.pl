adf501ca9ffe8c3ce3676ed6d0efc607e4472ce0
Дмитрий Губачев <gubachev@reg.ru>
2017-10-01 23:12:07 +0400

    В insert использован do и добавлен handler

--- server.pl
+++ server.pl
@@ -1,42 +1,40 @@
 #!/usr/bin/perl
 # Perl Web-Server
 
-# Подключить модули для
-use Modern::Perl; # ужесточения автоматического контроля
-use Socket; # использования сокета
-use BackendDb; # использования модуля бекенда
+use Modern::Perl;
+use Socket; # модуль для использования сокета
+use BackendDb;
 
-# Объявить переменные для хранения
-my @arguments = @ARGV; # аргументов из командной строки
-my $ip_address = $arguments[1]; # IP-адреса сервера
-my $port = $arguments[3]; # порта сервера
-my $data; # запросов и ответов
+my @arguments = @ARGV; # хранение аргументов из командной строки
+my $ip_address = $arguments[1]; # IP-адрес сервера
+my $port = $arguments[3]; # порт сервера
+my $data; # содержиоме запросов и ответов
 
-socket( SERVER, PF_INET, SOCK_STREAM, getprotobyname( 'tcp' ) ) # cоздать сокет с параметрами ИМЯ, ДОМЕН, ТИП, ПРОТОКОЛ
+socket( SERVER, PF_INET, SOCK_STREAM, getprotobyname( 'tcp' ) )
        or die "Can't open socket $!\n";
 
-bind( SERVER, pack_sockaddr_in( $port, inet_aton( $ip_address ) ) ) # привязать сокет к порту и IP-адресу хоста
+bind( SERVER, pack_sockaddr_in( $port, inet_aton( $ip_address ) ) )
     or die "Can't bind to port $port! \n";
 
-listen( SERVER, 5 ) or die "listen: $!"; # вызвать прослушивание входящих запросов на порту
+listen( SERVER, 5 ) or die "listen: $!";
 
-say "SERVER started on port $port"; # сообщение о запуске сервера с указанием порта
+say "SERVER started on port $port";
 
 while ( accept( CLIENT, SERVER ) ) { # принимать входящие соединения
-       chomp ( $data = <CLIENT> ); # получить запрос от клиента
+       chomp ( $data = <CLIENT> );
        
-       say "Получен запрос: $data"; # сообщение о получении запроса
+       say "Получен запрос: $data";
 
-       if ( $data =~ /(\/.*) H/ && $1 ne "favicon.ico" ) { # если не запрошен favicon.ico выделить URI из запроса
-            my @catalog = BackendDb::process_request( $1 ); # передать URL в метод process_request бекенда
-            responce_data( @catalog ); # сформировать и отправить ответ клиенту
+       if ( $data =~ /(\/.*) H/ && $1 ne "favicon.ico" ) { # если не запрошен favicon.ico выделяем URI и передаем в бекенд
+            my @catalog = BackendDb::process_request( $1 );
+            responce_data( @catalog );
        }
       
-       close CLIENT; # закрыть сокет клиента после отправки ответа
+       close CLIENT;
 }
 
 sub responce_data {
-    my ( @catalog ) = @_; # получить переданные аргументы
+    my ( @catalog ) = @_;
     if ( $catalog[0] eq "" ) { # если от бекенда получен пустой массив
          responce_404_error(); # отправить ответ с кодом состояния 404 Not Found
     }
@@ -48,31 +46,31 @@ sub responce_data {
                       my $rupture_counter = 0; # счетчик для отправки разрыва между позициями каталога
                       my $rupture_comparator = 5; # переменная для хранения частоты разрыва
 
-                      if ( $array1->{ "new_order" } ) { $rupture_comparator = 6; } # если есть ключ new_order увеличить частоту разрыва
+                      if ( $array1->{ "new_order" } ) { $rupture_comparator = 6; } # если есть ключ new_order увеличиваем частоту разрыва
 
-                      while ( ( my $key, my $value ) = each %$array1 ) { # перебрать все ключи и значения из хешей
-                                $rupture_counter++; # увеличить счетчика разрыва на 1
+                      while ( ( my $key, my $value ) = each %$array1 ) { # перебираем все ключи и значения из хешей
+                                $rupture_counter++; # увеличиваем счетчик разрыва на 1
 
                                 if ( $key eq "Creation" ) { # если ключ имеет имя Creation
-                                     my $hash1 = $array1->{ "Creation" }; # записать хеш по ключу Creation
+                                     my $hash1 = $array1->{ "Creation" }; # записываем хеш по ключу Creation
                                 
-                                     print CLIENT "<html><center><h1>Creation: </center></h1></html>"; # отправить содержимое Creation:
+                                     print CLIENT "<html><center><h1>Creation: </center></h1></html>"; # отправляем содержимое Creation:
                                
-                                     while ( ( my $key1, my $value1 ) = each %$hash1 ) { # перебрать все ключи и значения хеша Creation
-                                               print CLIENT "<html><center><h1>$key1: $value1</center></h1></html>";  # отправить ключи: значения хеша Creation
+                                     while ( ( my $key1, my $value1 ) = each %$hash1 ) { # перебираем все ключи и значения хеша Creation
+                                               print CLIENT "<html><center><h1>$key1: $value1</center></h1></html>";  # отправляем ключи: значения хеша Creation
                                      }
                                 }
                                 else { # если ключ не имеет имя Creation
-                                     print CLIENT "<html><center><h1>$key: $value</center></h1></html>"; # отправить содержимое остальных хешей
+                                     print CLIENT "<html><center><h1>$key: $value</center></h1></html>"; # отправляем содержимое остальных хешей
                                 }
                                 
                                 if ( $rupture_counter == $rupture_comparator ) { # если счетчик разрывов равен частоте разрывов
-                                     print CLIENT "<html><center><h1>---</center></h1></html>";  # отправить разрыв между позициями каталога
+                                     print CLIENT "<html><center><h1>---</center></h1></html>";  # отправляем разрыв между позициями каталога
                                 }
                       }
                  }
                  else { # если получено одно из сообщений
-                      print CLIENT "<html><center><h1>$array1</center></h1></html>"; # отправить содержимое с сообщением
+                      print CLIENT "<html><center><h1>$array1</center></h1></html>"; # отправляем содержимое с сообщением
                  }
          }
     }
